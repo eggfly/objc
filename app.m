4e0860d7f42a3f5e69f67add8b51ace48081c28d
eggfly <lihaohua90@gmail.com>
2015-07-09 14:21:17 +0800

    objc1
    Signed-off-by: eggfly <lihaohua90@gmail.com>

---
+++ app.m
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h> 
+int main(int argc, const char *argv[]) 
+{ 
+NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init]; 
+NSLog(@"hello www.jeapdu.com"); 
+NSLog(@"welcome to IOS Training Center!"); 
+[pool drain]; 
+return 0; 
+}
